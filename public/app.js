0d74385c8230ae18875bb10744e00448ea6ac32b
tech-shamp <shampytechnical@gmail.com>
2022-06-27 00:18:34 +0500

    Css Loader

---
+++ public/app.js
@@ -0,0 +1,41 @@
+// Declare Article Data as Json
+import data from "./articles.js";
+import header from "./components/header.js";
+
+// Components
+const Header = document.querySelector(".header");
+const Footer = document.querySelector(".footer");
+const DisplayPosts = document.querySelector(".blog");
+const loaderElem = document.querySelector(".loader");
+const Body = document.querySelector("body");
+
+// Header
+const HeaderElem = () => {
+  if (Header) {
+    Header.innerHTML += `${header}`;
+  } else console.error("Component Not Found || Is not working correctly");
+};
+setTimeout(HeaderElem, 3000);
+
+if (DisplayPosts) {
+  for (let i = 0; i < data.length; i++) {
+    const element = data[i];
+
+    DisplayPosts.innerHTML += `<div class="blog-card">
+    <div class="title">${element.title}</div>
+    <div class="subtitle">${element.subtitle}</div>
+    <div class="content">${element.content}</div>
+  </div>`;
+  }
+} else console.error(DisplayPosts);
+
+// Css Loader TimeOut
+Body.style.visibility = "hidden";
+loaderElem.style.visibility = "visible";
+
+const LoaderAnimation = () => {
+  Body.style.visibility = "visible";
+  loaderElem.style.visibility = "hidden";
+};
+
+setTimeout(LoaderAnimation, 3000);
---
+++ public/components/header.js
@@ -0,0 +1,13 @@
+const HeaderData = [
+  `<div class="logo">
+<a href="#" class="img">Technical Shampy</a>
+</div>
+<ul class="nav-list">
+<li><a class="nav-links" href="#">Home</a></li>
+<li><a class="nav-links" href="#">Blog</a></li>
+<li><a class="nav-links" href="#">Contact</a></li>
+<li><a class="nav-links" href="#">About</a></li>
+</ul>`,
+];
+
+export default HeaderData;
