ad52c23d72d5acae25f6a79572d6849b20984e7f
tech-shamp <shampytechnical@gmail.com>
2022-09-14 15:45:30 -0500

    pretied

--- public/app.js
+++ public/app.js
@@ -1,28 +1,28 @@
 // Declare Article Data as Json
-import data from "./js/articles.js";
-import header from "./components/header.js";
+import header from "./components/header.js"
+import data from "./js/articles.js"
 
 // Components
-const Header = document.querySelector(".header");
-const Footer = document.querySelector(".footer");
-const loaderElem = document.querySelector(".loader");
-const Body = document.querySelector("body");
+const Header = document.querySelector(".header")
+const Footer = document.querySelector(".footer")
+const loaderElem = document.querySelector(".loader")
+const Body = document.querySelector("body")
 
 // Header
 const HeaderElem = () => {
   if (Header) {
-    Header.innerHTML += `${header}`;
-  } else console.error("Component Not Found || Is not working correctly");
-};
-setTimeout(HeaderElem, 3000);
+    Header.innerHTML += `${header}`
+  } else console.error("Component Not Found || Is not working correctly")
+}
+setTimeout(HeaderElem, 3000)
 
 // Css Loader TimeOut
-Body.style.visibility = "hidden";
-loaderElem.style.visibility = "visible";
+Body.style.visibility = "hidden"
+loaderElem.style.visibility = "visible"
 
 const LoaderAnimation = () => {
-  Body.style.visibility = "visible";
-  loaderElem.style.visibility = "hidden";
-};
+  Body.style.visibility = "visible"
+  loaderElem.style.visibility = "hidden"
+}
 
-setTimeout(LoaderAnimation, 3000);
+setTimeout(LoaderAnimation, 3000)
--- public/js/workable.js
+++ public/js/workable.js
@@ -1,26 +1,26 @@
-const DisplayPosts = document.querySelector(".blog");
+const DisplayPosts = document.querySelector(".blog")
 
 // Displaying Posts
 if (DisplayPosts) {
   const addArticle = () => {
-    const title = document.querySelector(".title");
-    const subtitle = document.querySelector(".subtitle");
-    const content = document.querySelector(".content");
+    const title = document.querySelector(".title")
+    const subtitle = document.querySelector(".subtitle")
+    const content = document.querySelector(".content")
 
-    title.innerHTML += data[0].title;
-    subtitle.innerHTML += data[0].subtitle;
-    content.innerHTML += data[0].content;
+    title.innerHTML += data[0].title
+    subtitle.innerHTML += data[0].subtitle
+    content.innerHTML += data[0].content
 
-    console.log(data);
-  };
-  addArticle();
-} else console.error("Any Element not found having DisplayPosts Class.");
+    console.log(data)
+  }
+  addArticle()
+} else console.error("Any Element not found having DisplayPosts Class.")
 
 // Also This
-const title = document.querySelector(".title");
-const subtitle = document.querySelector(".subtitle");
-const content = document.querySelector(".content");
+const title = document.querySelector(".title")
+const subtitle = document.querySelector(".subtitle")
+const content = document.querySelector(".content")
 
-title.innerHTML += element.title;
-subtitle.innerHTML += element.subtitle;
-content.innerHTML += element.content;
+title.innerHTML += element.title
+subtitle.innerHTML += element.subtitle
+content.innerHTML += element.content
